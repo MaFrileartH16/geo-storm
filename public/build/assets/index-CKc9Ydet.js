2a748cdbf46e34b707b9903ca2e98b2c0ceec124
MaFrileartH16 <muhammadfauzulhanif16@gmail.com>
2025-01-26 06:23:42 +0700

    update

---
+++ public/build/assets/index-CKc9Ydet.js
@@ -0,0 +1,1 @@
+import{i as r,l as t}from"./app-CQq0w004.js";var a=r();const e=t(a);export{e as R,a as r};
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,9 +1,10 @@
-import '../css/app.css';
-import './bootstrap';
-
 import { createInertiaApp } from '@inertiajs/react';
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
+import '../css/app.css';
+import './bootstrap';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -17,7 +18,11 @@ createInertiaApp({
   setup({ el, App, props }) {
     const root = createRoot(el);
 
-    root.render(<App {...props} />);
+    root.render(
+      <MantineProvider>
+        <App {...props} />
+      </MantineProvider>,
+    );
   },
   progress: {
     color: '#4B5563',
